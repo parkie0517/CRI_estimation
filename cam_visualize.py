6136094ad6b61e08bd7f965f5a067ae71557f7c7
Heejun Park <parkie0517@gmail.com>
2024-12-08 16:37:42 +0900

    actually discard the code that are unnecessary

--- cam_visualize.py
+++ cam_visualize.py
@@ -43,74 +43,6 @@ class CRIDataset(Dataset):
 
 
 
-# Paths
-TRAIN_RGB_DIR = "./student_dataset/train/current_image"
-TRAIN_SEG_DIR = "./results_train/segmentation/filtered"
-#CLASS_WEIGHTS = [300, 100, 100, 80, 100]
-
-# Hyperparameters
-BATCH_SIZE = 128
-EPOCHS = 10
-LEARNING_RATE = 1e-4
-VAL_SPLIT = 0.3
-
-# Define Transformations
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor()
-])
-"""
-# Load Data
-datasets = []
-labels = []
-for label in range(5):
-    rgb_dir = os.path.join(TRAIN_RGB_DIR, f"cri_{label}")
-    seg_dir = os.path.join(TRAIN_SEG_DIR, f"cri_{label}")
-    files = os.listdir(rgb_dir)
-    datasets.append(CRIDataset(rgb_dir, seg_dir, [label] * len(files), transform))
-    labels.extend([label] * len(files))
-
-# Combine datasets
-combined_datasets = torch.utils.data.ConcatDataset(datasets)
-
-
-# Weighted Random Sampler
-weights = [1.0 / CLASS_WEIGHTS[label] for label in labels]
-sampler = WeightedRandomSampler(weights, len(weights), replacement=True)
-
-
-"""
-
-
-"""
-# Split dataset
-val_size = int(VAL_SPLIT * len(combined_datasets))
-train_size = len(combined_datasets) - val_size
-train_dataset, val_dataset = random_split(combined_datasets, [train_size, val_size]) # [476, 204]
-
-
-############
-train_labels = []
-for idx in range(len(train_dataset)):
-    _, label = train_dataset[idx]
-    train_labels.append(label)
-    
-
-from collections import Counter
-label_counts = Counter(train_labels)
-total_samples = len(train_labels)
-class_weights = {label: total_samples / count for label, count in label_counts.items()}
-# Assign weights to each sample based on its label
-sample_weights = [class_weights[label] for label in train_labels]
-
-
-sampler = WeightedRandomSampler(weights=sample_weights, num_samples=len(sample_weights), replacement=True)
-############
-
-
-train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, sampler=sampler)
-#val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
-"""
 
 
 # Model
@@ -141,66 +73,7 @@ class CRIModel(nn.Module):
         x = self.fc2(x)
         return x 
     
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = CRIModel().to(device)
-
-
-"""
-
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
-
-# Training Loop
-for epoch in range(EPOCHS):
-    model.train()
-    running_loss = 0.0
     
-    for batch in train_loader:
-        data, labels = batch
-        rgb = data["rgb"].to(device)
-        seg = data["seg"].to(device)
-        labels = labels.to(device)
-        
-        optimizer.zero_grad()
-        outputs = model(rgb, seg)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item()
-        
-    scheduler.step()
-    print(f"Epoch {epoch+1}, Loss: {running_loss/len(train_loader)}")
-    
-    
-# Save the model checkpoint after each epoch
-checkpoint = {
-    'model_state_dict': model.state_dict(),
-    'optimizer_state_dict': optimizer.state_dict(),
-    'scheduler_state_dict': scheduler.state_dict(),
-    'loss': running_loss / len(train_loader)
-}
-torch.save(checkpoint, f'checkpoint_{EPOCHS}epoch.pth')
-print("Done saving")
-"""
-
-
-# Load the model checkpoint
-checkpoint = torch.load('checkpoint_epoch_10.pth')  # Replace with your checkpoint file
-model.load_state_dict(checkpoint['model_state_dict'])
-model = model.to(device)
-
-# Set the model to evaluation mode if you are using it for inference
-model.eval()  # Or model.train() if you want to resume training
-breakpoint()
-# Transform
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor()
-])
-
-
 
 # Function to generate and save CAM
 def generate_and_save_cam(model, image_tensor, save_path, target_class=None):
@@ -261,6 +134,37 @@ def generate_and_save_cam(model, image_tensor, save_path, target_class=None):
     
     # Remove the hook
     hook.remove()
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = CRIModel().to(device)
+
+"""
+# Save the model checkpoint after each epoch
+checkpoint = {
+    'model_state_dict': model.state_dict()
+}
+torch.save(checkpoint, f'checkpoint_10epoch.pth')
+print("Done saving")
+"""
+
+
+# Load the model checkpoint
+checkpoint = torch.load('checkpoint_10epoch.pth')  # Replace with your checkpoint file
+model.load_state_dict(checkpoint['model_state_dict'])
+model = model.to(device)
+
+# Set the model to evaluation mode if you are using it for inference
+model.eval()  # Or model.train() if you want to resume training
+
+
+# Transform
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor()
+])
+
+
 
 # After training or during evaluation
 # Example: Generating CAM for a sample test image
